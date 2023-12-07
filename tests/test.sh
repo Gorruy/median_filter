08136bcd20315bdb713fbeb62166a835692a587d
gorruy <vladimirmimikin@gmail.com>
2023-12-08 01:49:56 +0300

    shell scrtip exit 1 inside pipe fixed

--- tests/test.sh
+++ tests/test.sh
@@ -1,52 +1,54 @@
 #!/usr/bin/bash
-# Тест всей программы
-gcc ../tests/test_main.c ../src/median_filter.c -o test_main;
+# Тест всей программ
+
+set -e
+
 cycles=10000;
 window_size=10;
 let res=$cycles/$window_size+1;
 ./test_main $cycles | ../bin/median_filter $window_size > test_main.txt;
 
-grep -o '[^[:space:]]*' test_main.txt | sort -u | wc -l| (read words &&
+grep -o '[^[:space:]]*' test_main.txt | sort -u | wc -l| { read words &&
 if [[ $(($words)) -ne $res ]] 
 then
     exit 1
-fi);
+fi }
 
-echo "1 10101001 11111111111111111111111111111111 1" | ../bin/median_filter 2 | (read res &&
-if [[ $res != "1010101 1010101 0 0" ]] 
-then 
-    exit 1
-fi);
+echo "1 10101001 11111111111111111111111111111111 1" | ../bin/median_filter 2 | { read res && 
+if [[ $res != "1010101 1010101 0 0" ]];
+then
+    exit 1;
+fi }
 
-echo "100 11111 100101 1010110 1010100 1010101 11111 11111110110111101111110111000011" | 
-    ../bin/median_filter 3 | (read res && 
+echo "100 11111 100101 1010110 1010100 1010101 11111 11111110110111101111110111000011" | ../bin/median_filter 3 | { read res && 
 if [[ $res != "11111 11111 11111 1010101 1010101 1010101 11111111011011110111111011110001 11111111011011110111111011110001" ]] 
-then 
+then
     exit 1
-fi);
+fi }
 
-echo "1 1 1 1001 10010 10101010 11010 10101010 111 0 0 0 0 1 1 1" | ../bin/median_filter 1 | (read res &&
+echo "1 1 1 1001 10010 10101010 11010 10101010 111 0 0 0 0 1 1 1" | ../bin/median_filter 1 | { read res &&
 if [[ $res != "1 1 1 1001 10010 10101010 11010 10101010 111 0 0 0 0 1 1 1" ]] 
 then 
     exit 1
-fi);
+fi }
 
-echo "1 1 1 10101010101010" | ../bin/median_filter | (read res &&
+echo "1 1 1 10101010101010" | ../bin/median_filter | { read res &&
 if [[ $res != "Wrong usage!" ]] 
 then 
     exit 1
-fi);
+fi }
 
-echo "1 1 1 10101010101010" | ../bin/median_filter -10| (read res &&
+echo "1 1 1 10101010101010" | ../bin/median_filter -10| { read res &&
 if [[ $res != "Wrong argument: out of range!" ]] 
 then 
     exit 1
-fi);
+fi }
 
-echo "1 1 1 10101010101010" | ../bin/median_filter 13120312312312312312351234135| (read res &&
+echo "1 1 1 10101010101010" | ../bin/median_filter 13120312312312312312351234135| { read res &&
 if [[ $res != "Wrong argument: out of range!" ]] 
 then 
     exit 1
-fi);
+fi }
 
-exit 0;
+exit 0
+
--- tests/test_print_bin.sh
+++ tests/test_print_bin.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
 #Тест модуля вывода данных в бинарном формате
-gcc ../tests/test_print_bin.c ../src/median_filter.c -o test_print;
-
+rm -f *.txt;
 printer='10101000 1010101010 111 1 1111111 1111111 110101010 11111111111111111111111111111111 10000000000000000000000000000000';
 counter=9;
 echo $printer > test_print_bin.txt && echo $printer | ./test_print $counter;
